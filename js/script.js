48df62968b763b581099287a9f9402dfb898f226
Salaudeen Ismail <ismailsalaudeen6696@gmail.com>
2020-06-10 03:24:27 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+var playBtn = document.querySelector("button[type=submit]");
+var choiceTopic = document.querySelector("#choice")
+
+
+playBtn.addEventListener("click", function load() {
+    if (choiceTopic.value !== "select") {
+        //This should take the choice topic value and send it to whatever handles the API AJAX Request...
+
+
+    }
+});
